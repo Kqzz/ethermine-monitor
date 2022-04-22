60270c3fae646282764d4afc85d9710aba676c87
Kqzz <48300938+Kqzz@users.noreply.github.com>
2022-04-21 21:45:10 -0400

    add image code

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/kqzz/ethermine-monitor
 
 go 1.17
+
+require github.com/wcharczuk/go-chart/v2 v2.1.0
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
+	"time"
+
+	chart "github.com/wcharczuk/go-chart/v2"
+	"github.com/wcharczuk/go-chart/v2/drawing"
 )
 
 type Dashboard struct {
@@ -58,6 +64,10 @@ func GetDashboard(address string) (Dashboard, error) {
 		return dashboard, err
 	}
 
+	if res.StatusCode != 200 {
+		return dashboard, fmt.Errorf("status code %d", res.StatusCode)
+	}
+
 	defer res.Body.Close()
 
 	err = json.NewDecoder(res.Body).Decode(&dashboard)
@@ -77,17 +87,103 @@ func HashrateToMhs(hashrate float64) float64 {
 	return hashrate / 1000000
 }
 
-func main() {
-	address := "0xf87ca7d6b9ac4f656fa73b180bfe3c42b38eede1"
-	dashboard, err := GetDashboard(address)
+func GetChartSeries(dashboard Dashboard) []chart.Series {
+	hashrates := chart.TimeSeries{
+		Name: "Current Hashrate",
+		Style: chart.Style{
+			StrokeColor: drawing.ColorFromHex("1f77b4"),
+		},
+	}
 
+	reportedHashrates := chart.TimeSeries{
+		Name: "Reported Hashrate",
+		Style: chart.Style{
+			StrokeColor: drawing.ColorFromHex("35c335"),
+		},
+	}
+
+	for _, stat := range dashboard.Data.Statistics {
+		hashrates.XValues = append(hashrates.XValues, time.Unix(int64(stat.Time), 0))
+		reportedHashrates.XValues = append(reportedHashrates.XValues, time.Unix(int64(stat.Time), 0))
+
+		hashrates.YValues = append(hashrates.YValues, HashrateToMhs(stat.CurrentHashrate))
+		reportedHashrates.YValues = append(reportedHashrates.YValues, HashrateToMhs(stat.ReportedHashrate))
+	}
+
+	return []chart.Series{hashrates, reportedHashrates}
+}
+
+func GetGraph(dashboard Dashboard) *bytes.Buffer {
+	graph := chart.Chart{
+		Width:  900,
+		Height: 200,
+		YAxis: chart.YAxis{
+			Name: "MH/S",
+			NameStyle: chart.Style{
+				FontColor: drawing.ColorWhite,
+			},
+			Style: chart.Style{
+				StrokeColor: drawing.ColorWhite,
+				FontColor:   drawing.ColorWhite,
+			},
+		},
+		XAxis: chart.XAxis{
+			Name:           "Time",
+			ValueFormatter: chart.TimeHourValueFormatter,
+			NameStyle: chart.Style{
+				FontColor: drawing.ColorWhite,
+			},
+			Style: chart.Style{
+				StrokeColor: drawing.ColorWhite,
+				FontColor:   drawing.ColorWhite,
+			},
+		},
+		Canvas: chart.Style{
+			FillColor: drawing.ColorFromHex("262327"),
+			FontColor: drawing.ColorWhite,
+		},
+		Background: chart.Style{
+			FillColor: drawing.ColorFromHex("262327"),
+		},
+		Series: GetChartSeries(dashboard),
+	}
+
+	buff := bytes.NewBuffer([]byte{})
+	err := graph.Render(chart.PNG, buff)
 	if err != nil {
 		panic(err)
 	}
+	return buff
+}
 
-	fmt.Printf("%v\n", address)
-	fmt.Printf("%.0f / %d workers running\n", dashboard.Data.CurrentStatistics.ActiveWorkers, len(dashboard.Data.Workers))
-	fmt.Printf("%f ETH unpaid\n", WeiToEther(dashboard.Data.CurrentStatistics.Unpaid))
-	fmt.Printf("%f MH/S\n", HashrateToMhs(dashboard.Data.CurrentStatistics.CurrentHashrate))
+func main() {
+
+	var interval float64
+
+	flag.Float64Var(&interval, "interval", 5, "Interval in minutes")
+
+	flag.Parse()
+
+	address := flag.Arg(0)
+
+	if address == "" {
+		panic("no address given")
+	}
+
+	for {
+
+		dashboard, err := GetDashboard(address)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("%v\n", address)
+		fmt.Printf("%.0f / %d workers running\n", dashboard.Data.CurrentStatistics.ActiveWorkers, len(dashboard.Data.Workers))
+		fmt.Printf("%f ETH unpaid\n", WeiToEther(dashboard.Data.CurrentStatistics.Unpaid))
+		fmt.Printf("%f MH/S\n", HashrateToMhs(dashboard.Data.CurrentStatistics.CurrentHashrate))
+
+		time.Sleep(time.Duration(interval) * time.Minute)
+	}
 
 }
